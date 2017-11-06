66c294efb3d483303968e96aa34119a98af6cef9
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-06 17:44:24 +0300

    gendiff file is fixed

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,8 +4,6 @@ import program from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option("-h, --help", "output usage information")
-  .option("-V, --version", "output the version number")
   .option("-f, --format [type]", "output format")
   .arguments('<firstArgument> <secondArgumant>')
   .parse(process.argv);
