67c301932bd555fe26559d9faa8357f05c22fe4c
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-06 20:12:20 +0300

    json file updated

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import program from 'commander';
+import comparator from '..';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option("-f, --format [type]", "output format")
-  .arguments('<firstArgument> <secondArgumant>')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstArgument> <secondArgument>')
+  .action((firstArgument, secondArgument) => {
+    console.log(comparator(firstArgument, secondArgument));
+  })
   .parse(process.argv);
 
 if (!program.args.length) program.help();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import fs from 'fs';
+//
+//
+//
+// export default (firstFile, secondFile) => {
+//   const getFistFileObject
+//   const getSecondFileObject
+//   const compareFiles
+// };
