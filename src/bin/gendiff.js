2417f849b2042e900af04abd4a17e2964b0c972c
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-06 14:34:53 +0300

    the project environment is created

---
+++ src/bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import commander from 'commander'
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option("-h, --help", "output usage information")
+  .option("-V, --version", "output the version number")
+  .option("-f, --format [type]", "output format")
+  .arguments('<firstArgument> <secondArgumant>')
+  .parse(process.argv);
+
+if (!program.args.length) program.help();
+
+// console.log('Do me a favour');
