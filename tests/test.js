753d784844d6d03c8b3f9dc4ecda53a939ca010e
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-07 21:52:09 +0300

    fixed

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,11 @@
 import genDiff from '../src';
 
-const firstFilePath = './__tests__/fixtures/before.json';
-const secondFilePath = './__tests__/fixtures/after.json';
-
-const actual = genDiff(firstFilePath, secondFilePath);
-const expected = `{
+test('compare ini', () => {
+  expect(genDiff('./tests/fixtures/before.json', './tests/fixtures/after.json')).toBe(`{
     host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`;
-
-test('compare result and exprected values', () => {
-  expect(actual).toBe(expected);
+    + timeout: 20
+    - timeout: 50
+    - proxy: 123.234.53.22
+    + verbose: true
+}`);
 });
