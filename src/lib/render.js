143193ef5e80fe629b64af8d26ea384d84e98826
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-11 03:50:24 +0300

    the lib is created

---
+++ src/lib/render.js
@@ -0,0 +1,36 @@
+const space = '  ';
+
+const render = (ast, shift = 1) => {
+  const doubleShift = space.repeat(shift);
+  const newShift = shift + 2;
+  const result = ast.map((obj) => {
+    if (obj.operation === ' ') {
+      if (!obj.children.length) {
+        return `${doubleShift}  ${obj.key}: ${obj.secondVal}\n`;
+      }
+      return `${doubleShift} ${obj.key}: ${render(obj.children, newShift)}\n`;
+    }
+    if (obj.operation === '+-') {
+      if (!obj.children.length) {
+        return `${doubleShift}+ ${obj.key}: ${obj.secondVal}\n${doubleShift}- ${obj.key}: ${obj.firstVal}\n`;
+      }
+      return `${doubleShift}+ ${obj.key}: ${render(obj.children, newShift)}\n`;
+    }
+    if (obj.operation === '-') {
+      if (!obj.children.length) {
+        return `${doubleShift}- ${obj.key}: ${obj.firstVal}\n`;
+      }
+      return `${doubleShift}- ${obj.key}: ${render(obj.children, newShift)}\n`;
+    }
+    if (obj.operation === '+') {
+      if (!obj.children.length) {
+        return `${doubleShift}+ ${obj.key}: ${obj.secondVal}\n`;
+      }
+      return `${doubleShift}+ ${obj.key}: ${render(obj.children, newShift)}\n`;
+    }
+    return 'No combinations is match';
+  });
+  return `{\n${result.join('')} }`;
+};
+
+export default render;
