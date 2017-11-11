24f695e77474294c186a65006796d27c2234a827
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-11 04:25:16 +0300

    fixed

--- tests/test.js
+++ tests/test.js
@@ -23,3 +23,51 @@ describe('genDiff', () => {
     expect(actual).toBe(expected);
   });
 });
+
+describe('Compares two Recursive files', () => {
+  const expected =
+`{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+      - setting6: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
+}`;
+
+  test('compare .json files with recursion', () => {
+    const path1 = 'tests/fixtures/before2.json';
+    const path2 = 'tests/fixtures/after2.json';
+    const actual = genDiff(path1, path2);
+    expect(actual).toBe(expected);
+  });
+  test('compare .yaml files with recursion', () => {
+    const path1 = 'tests/fixtures/before2.yml';
+    const path2 = 'tests/fixtures/after2.yml';
+    const actual = genDiff(path1, path2);
+    expect(actual).toBe(expected);
+  });
+  test('compare .ini files with recursion', () => {
+    const path1 = 'tests/fixtures/before2.ini';
+    const path2 = 'tests/fixtures/after2.ini';
+    const actual = genDiff(path1, path2);
+    expect(actual).toBe(expected);
+  });
+});
