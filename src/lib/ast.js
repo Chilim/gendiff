a1efd59f1260936478e6ad90560717edd5f9241b
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-12 03:48:28 +0300

    fixed

--- src/lib/ast.js
+++ src/lib/ast.js
@@ -8,15 +8,15 @@ const getDiff = (firstObj, secondObj) => _.union(_.keys(firstObj), _.keys(second
   .map((key) => {
     if (!_.has(firstObj, key) && _.has(secondObj, key)) {
       if (_.isObject(secondObj[key])) {
-        return makeObj({ key, children: getDiff(secondObj[key], secondObj[key]), operation: 'add' });
+        return makeObj({ key, children: getDiff(secondObj[key], secondObj[key]), operation: 'added' });
       }
-      return makeObj({ key, secondVal: secondObj[key], operation: 'add' });
+      return makeObj({ key, secondVal: secondObj[key], operation: 'added' });
     }
     if (_.has(firstObj, key) && !_.has(secondObj, key)) {
       if (_.isObject(firstObj[key])) {
-        return makeObj({ key, children: getDiff(firstObj[key], firstObj[key]), operation: 'delete' });
+        return makeObj({ key, children: getDiff(firstObj[key], firstObj[key]), operation: 'deleted' });
       }
-      return makeObj({ key, firstVal: firstObj[key], operation: 'delete' });
+      return makeObj({ key, firstVal: firstObj[key], operation: 'deleted' });
     }
     if (!_.isObject(firstObj[key]) && !_.isObject(secondObj[key])) {
       if (firstObj[key] === secondObj[key]) {
