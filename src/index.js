8972ad15a39bab33a11651870a4ac14e672d1fa4
Davud Kakhrimanov <davud.kakhriman@gmail.com>
2017-11-12 03:47:58 +0300

    render formats oprion applied

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 import getDiff from './lib/ast';
-import render from './lib/render';
+import renderToPlain from './lib/render_formats/renderToPlain';
+import renderToString from './lib/render_formats/renderToString';
+
+const renderFormats = {
+  plain: renderToPlain,
+  casual: renderToString,
+};
 
 const formatActions = {
   '.json': JSON.parse,
@@ -13,14 +19,15 @@ const formatActions = {
 
 const getExtenAction = exten => formatActions[exten];
 
-const genDiff = (firstFilePath, secondFilePath) => {
+const genDiff = (firstFilePath, secondFilePath, renderFormat = 'plain') => {
   const firstObj = fs.readFileSync(firstFilePath, 'utf8');
   const secondObj = fs.readFileSync(secondFilePath, 'utf8');
   const exten = path.extname(firstFilePath);
   const extenAction = getExtenAction(exten);
   const firstFileContent = extenAction(firstObj);
   const secondFileContent = extenAction(secondObj);
-  return render(getDiff(firstFileContent, secondFileContent));
+  const ast = getDiff(firstFileContent, secondFileContent);
+  return renderFormats[renderFormat](ast);
 };
 
 export default genDiff;
