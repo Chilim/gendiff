4c31054972528796c3e1c01c2c09610d10ee80df
Davud <davud@comp.fritz.box>
2019-05-24 00:13:07 +0300

    optimized

--- src/ast.js
+++ src/ast.js
@@ -1,25 +1,20 @@
-const _ = require('lodash');
+import _ from 'lodash';
 
-const makeObj = ({ key, oldVal, newVal, type }) => {
-  const obj = { key, oldVal, newVal, type };
-  return obj;
-};
+const diffType = [
+  { type: 'hasChild', check: (val1, val2) => _.isObject(val1) && _.isObject(val2) },
+  { type: 'unchanged', check: (val1, val2) => val1 === val2 },
+  { type: 'changed', check: (val1, val2) => (val1 && val2) },
+  { type: 'deleted', check: (val1, val2) => (val2 === undefined) },
+  { type: 'added', check: val1 => (val1 === undefined) },
+];
 
-const getDiff = (firstObj, secondObj) => _.union(_.keys(firstObj), _.keys(secondObj))
-  .map((key) => {
-    if (_.isObject(firstObj[key]) && _.isObject(secondObj[key])) {
-      return makeObj({ key, oldVal: getDiff(firstObj[key], secondObj[key]), type: 'hasChildren' });
-    }
-    if (_.has(firstObj, key) && !_.has(secondObj, key)) {
-      return makeObj({ key, oldVal: firstObj[key], type: 'deleted' });
-    }
-    if (!_.has(firstObj, key) && _.has(secondObj, key)) {
-      return makeObj({ key, newVal: secondObj[key], type: 'added' });
-    }
-    if (_.has(firstObj, key) && _.has(secondObj, key) && firstObj[key] === secondObj[key]) {
-      return makeObj({ key, oldVal: firstObj[key], type: 'unchanged' });
-    }
-    return makeObj({ key, oldVal: firstObj[key], newVal: secondObj[key], type: 'changed' });
-  });
+const getType = (arg1, arg2) => _.find(diffType, ({ check }) => check(arg1, arg2));
 
-export default getDiff;
+const buildAst = (before, after) => _.union(_.keys(before), _.keys(after))
+  .reduce((acc, key) => {
+    const { type } = getType(before[key], after[key]);
+    const children = (type === 'hasChild') ? buildAst(before[key], after[key]) : [];
+    return [...acc, { key, type, oldValue: before[key], newValue: after[key], children }];
+  }, []);
+
+  export default buildAst;
